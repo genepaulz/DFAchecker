49e744f6d753a8076add584ab495a70a48e3e5d7
genepaulz <zafra.genepaulm2013@gmail.com>
2020-11-23 21:26:46 +0800

    base

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[])
+{
+//	char a[2047];
+//	char *tok;
+//	const char s[2] = " ";
+//	scanf("%[^\n]s",a);
+//	tok = strtok(a,s);
+//	while(tok!=NULL)
+//		{
+//			printf("%s\n ",tok);
+//			tok = strtok(NULL," ");
+//		}
+//	int *x;
+//	x=10;
+//	printf("%d",x);
+	return 0;
+}
